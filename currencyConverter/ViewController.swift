7a20371e2a0c0e774d329b117572d67ef63a12ec
Tyra Neftzger <tyraneftzger@gmail.com>
2021-12-16 09:33:41 -0600

    Initial Commit

---
+++ currencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  currencyConverter
+//
+//  Created by Tyra Elliott Neftzger on 12/16/21.
+//  Copyright © 2021 Tyra Elliott Neftzger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
